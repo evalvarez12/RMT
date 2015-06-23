cf4a05f6af9557d5d68d162f98f23e245e1575ac
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-06-23 13:51:24 -0500

    Cambios

--- nested.py
+++ nested.py
@@ -1,24 +1,36 @@
 from pylab import * 
 import rmt
 
+
+def H(self) :
+  return self.conjugate().T
+
+def GUE(dim) :
+  A=randn(dim,dim) + 1j*randn(dim,dim)
+  return sqrt(1/(4.*dim))*( A + H(A)) 
+  
+  
+
+
+
 def Hamiltonian_CBA(dim_c,dim_b,dim_a,theta,gamma,lamb) :
   Ha=GUE(dim_a)
   Hb=GUE(dim_b)
   Hc=identity(dim_c)
   Vab=GUE(dim_a*dim_b)
-  Vbc=GUE(dim_b*dim_a)
+  Vbc=GUE(dim_b*dim_c)
   Ia=identity(dim_a)
   Ib=identity(dim_b)
   Ic=identity(dim_c)
   
-  Henv=cos(theta)*kron(Ha,Ib)+kron(Hb,na)+sin(theta)*gamma*Vab
+  Henv=cos(theta)*kron(Ha,Ib)+kron(Hb,Ia)+sin(theta)*gamma*Vab
   H=kron(Hc,identity(dim_a*dim_b))+ kron(Henv,Ic)+ lamb*kron(Vbc,Ia)
   return H
 
 
-nc=1
+nc=2
 na=8
 nb=6
-theta=0
+lamb=0.1
 
-H=Hamiltonian_CBA(nc,nb,na,)
+H=Hamiltonian_CBA(nc,nb,na,0,0,lamb)
--- rmt.jl
+++ rmt.jl
@@ -1,32 +1,11 @@
 #RMT in julia
 #Eduardo Villaseñor - evalvarez12@gmail.com
 
-#   itpp::Mat<std::complex<double> > RandomGUE(int const dim, std::string normalization="sigma_offdiag=1", double const percentage_away=0.1){ //{{{
-#     if (normalization=="sigma_offdiag=1"){
-#       return RandomGUEDeltaOne(dim);
-#     }	
-#     else if (normalization=="unfolded mean_level_spacing=1"){
-#       itpp::Mat<std::complex<double> > U(dim, dim), tmp(dim,dim);
-#       itpp::Vec<std::complex<double> > vec1(dim);
-#       itpp::Vec<double> eigenvalues(dim);
-#       FlatSpectrumGUE(U, eigenvalues);
-#       for (int i=0; i<dim; i++){
-#         vec1=itpp::elem_mult(conj(U.get_col(i)), to_cvec(eigenvalues));
-#         for (int j=i; j<dim; j++){
-#           tmp(i,j)=vec1*U.get_col(j);
-#           if (i<j){tmp(j,i)=conj(tmp(i,j));}
-#         }
-#       }
-#       return tmp;
-#       
-#       
-#       
-#         itpp::Mat<std::complex<double> > RandomGUEDeltaOne(int const dim){ //{{{
-#     itpp::Mat<std::complex<double> > temp(dim, dim);
-#     temp=itpp::randn_c(dim,dim);
-#     return sqrt(1/2.)*(temp+temp.hermitian_transpose());
-#   }
 
-function RandomGUEDeltaOne(dim) 
-  A= random(dim,dim)
+function RandomGUE(n::Integer) 
+  A = randn(n, n) + im*randn(n, n)
+  normalization = sqrt(4*n)
+  return (A + A') / normalization
+end
   
+ 
