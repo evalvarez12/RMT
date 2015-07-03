b18f6ab3ea9af2385f394994aebe217cb264f97e
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-07-02 20:27:33 -0500

    Grafica 1

--- nested.py
+++ nested.py
@@ -3,45 +3,52 @@ import rmt_py as rmt
 
 
 
-#def one_run(Ham_func,nc,nb,nc,lamb,time) :
-  #H=Ham_func(nc,nb,na,lamb)
-  #E,W=eig(H)
-  #state=kron(rmt.BasisState(nc),kron(rmt.RandState(nb),rmt.RandState(na)))
-  #stateW=dot(W.conjugate().transpose(),state)
-  #for i in range(time) :
-    #state_t=dot(W,exp(-1j*E*i)*stateW)
-    ##print real(rmt.purity_C(state_t))
-    #purs.append(real(rmt.purity_C(state_t)))
+def one_run(Ham_func,nc,nb,na,lamb,time) :
+  H=Ham_func(nc,nb,na,lamb)
+  E,W=eig(H)
+  state=kron(rmt.BasisState(nc),kron(rmt.RandState(nb),rmt.RandState(na)))
+  stateW=dot(W.conjugate().transpose(),state)
+  purities=[]
+  for i in range(time) :
+    state_t=dot(W,exp(-1j*E*i)*stateW)
+    #print real(rmt.purity_C(state_t))
+    purities.append(rmt.purity_C(state_t).real)
+  return array(purities)
+
+def one_run_fixed_t(Ham_func,nc,nb,na,lamb,time) :
+  H=Ham_func(nc,nb,na,lamb)
+  E,W=eig(H)
+  state=kron(rmt.BasisState(nc),kron(rmt.RandState(nb),rmt.RandState(na)))
+  stateW=dot(W.conjugate().transpose(),state)
+  purities=[]
+  state_t=dot(W,exp(-1j*E*time)*stateW)
+  return rmt.purity_C(state_t).real
+
 
 nc=2
-na=10
-nb=6
-lamb=.1
+na=2**10
+nb=2**6
 
-time=1000
 
-PS=zeros(time)
+time=2500
 
+lines=['o-','<-','p-','v-','+-','^-','h-','D-','>-','H-','d-','s-','x-','*-']
+li=0
 ave=50
 for lamb in [.001,.01,.05,.1,.5] :
+  PS=zeros(time)
   for j in range(ave) :
-    purs=[]
-    H=rmt.Hamiltonian_CBA(nc,nb,na,lamb)
-    E,W=eig(H)
-    state=kron(rmt.BasisState(nc),kron(rmt.RandState(nb),rmt.RandState(na)))
-    stateW=dot(W.conjugate().transpose(),state)
-    for i in range(time) :
-      state_t=dot(W,exp(-1j*E*i)*stateW)
-      #print real(rmt.purity_C(state_t))
-      purs.append(real(rmt.purity_C(state_t)))
+    purs=one_run(rmt.Hamiltonian_CBA,nc,nb,nc,lamb,time)
     PS+=purs
-    print j
     
+  print lamb  
   PS=PS/double(ave)
-  plot(PS,label="$"+str(lamb)+"$")
-
-
-label
-legend()
+  plot(PS,lines[ls],markevery=50,label="$\lambda="+str(lamb)+"$",)
+  ls+=1
+  
+  
+ylabel("$P$",fontsize=28)
+xlabel("$t$",fontsize=28)
+legend(loc='lower left'fontsize=25)
 show()
 
