38d6b1aebae2c049326afce0978c9170db47be7c
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-07-21 01:05:18 -0500

    Averaged

--- main.jl
+++ main.jl
@@ -4,16 +4,21 @@
 
 
 function one_run(Hamiltonian_func,nb,na,lamb,time) 
-  state=kron(BasisState(2),kron(RandState(nb),RandState(na)))
-  H=Hamiltonian_func(nb,na,lamb)
-  E,W=eig(H)
-  state=(W')*state
-  purs=[]
-  for i in range(0,time)
-    state_t=W*(exp(-1*im*E*i).* state)
-    append!(purs,[real(purity(state_t))])
-  end
-  return purs
+  ave=50
+  P_ave=zeros(time)
+  for i in range(0,ave)
+    state=kron(BasisState(2),kron(RandState(nb),RandState(na)))
+    H=Hamiltonian_func(nb,na,lamb)
+    E,W=eig(H)
+    state=(W')*state
+    purs=[]
+    for i in range(0,time)
+      state_t=W*(exp(-1*im*E*i).* state)
+      append!(purs,[real(purity(state_t))])
+    end
+    P_ave+=purs
+  end  
+  return P_ave/Float64(ave)
 end
 
 
@@ -21,7 +26,7 @@ na=2^8
 nb=2^4
 lamb=.01
 
-time=10
+time=2000
 
 p=one_run(Hamiltonian_CBA,nb,na,lamb,time) 
 writedlm("g1_p_lamb=.01_na=2^8_nb=2^4.dat",p)
